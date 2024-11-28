e2da094a29c2fdae19d5cda27cca0d2e82c70c29
Md. Masrafi Mondol <146985678+devMasrafi@users.noreply.github.com>
2024-11-28 12:05:25 +0600

    navBar update

--- src/components/navBar/page.jsx
+++ src/components/navBar/page.jsx
@@ -24,40 +24,50 @@ const NavBar = () => {
         scrollValue ? "bg-gray-800" : "bg-transparent"
       }`}
     >
-      <section className="container mx-auto flex justify-between">
+      <nav className="container mx-auto flex justify-between">
+        {/* Logo Section */}
         <div>
           {scrollValue ? (
             <Link href="/">
-              <img src="/images/devMasrafiwhite.svg" alt="logo" />
+              <img
+                src="/images/devMasrafiwhite.svg"
+                alt="Developer Masrafi logo - white"
+              />
             </Link>
           ) : (
             <Link href="/">
-              <img src="/images/devMasrafi.svg" alt="logo" />
+              <img src="/images/devMasrafi.svg" alt="Developer Masrafi logo" />
             </Link>
           )}
         </div>
+
+        {/* Links */}
         <div className="flex lg:gap-x-8 items-center">
-          <ul className="flex lg:gap-x-6 lg:text-xl lg:uppercase ">
-            <li className={`cursor-pointer lg:font-semibold  ${
-              scrollValue ? 'text-white' : ''
-            } `}>
+          <ul className="flex lg:gap-x-6 lg:text-xl lg:uppercase">
+            <li
+              className={`cursor-pointer lg:font-semibold ${
+                scrollValue ? "text-white" : ""
+              }`}
+            >
               works
             </li>
-            <li className={`cursor-pointer lg:font-semibold  ${
-              scrollValue ? 'text-white' : ''
-            } `}>
+            <li
+              className={`cursor-pointer lg:font-semibold ${
+                scrollValue ? "text-white" : ""
+              }`}
+            >
               blog
             </li>
           </ul>
           <button
             className={`lg:py-2 lg:px-6 lg:rounded-lg lg:text-xl lg:uppercase lg:font-semibold hover:bg-darkBlue transition-all duration-300 ease-in-out hover:text-skyblue ${
-              scrollValue ? "bg-skyblue text-darkBlue " : "text-darkBlue"
-            }  `}
+              scrollValue ? "bg-skyblue text-darkBlue" : "text-darkBlue"
+            }`}
           >
             lets talk
           </button>
         </div>
-      </section>
+      </nav>
     </main>
   );
 };
